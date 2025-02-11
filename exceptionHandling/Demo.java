2cfbf9a463e20cb42971116976178d759ed58495
VEDANT B <mevedantbhojne@gmail.com>
2025-02-12 03:08:09 +0530

    Delete exceptionHandling directory

--- exceptionHandling/Demo.java
+++
@@ -1,7 +0,0 @@
-package com.java.exceptionHandling;
-
-public class Demo {
-    public static void main(String[] args) {
-        Main.divide(3, 0); // throws an exceptions
-    }
-}
--- exceptionHandling/MyException.java
+++
@@ -1,8 +0,0 @@
-package com.java.exceptionHandling;
-
-public class MyException extends Exception{
-// By this we are able to call our own exceptions
-    public MyException(String message) {
-        super(message);
-    }
-}
