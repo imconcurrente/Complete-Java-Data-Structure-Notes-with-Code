aca9fe27cbe853b9cc3ca964c56982dab87e8fb5
VEDANT B <mevedantbhojne@gmail.com>
2025-02-12 02:39:11 +0530

    Delete JAVA directory

--- JAVA/src/com/java/JavaFirstProgram.java
+++
@@ -1,8 +0,0 @@
-package com.java;
-
-public class JavaFirstProgram {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-    }
-}
--- JAVA/src/com/java/Strings_03.java
+++
@@ -1,9 +0,0 @@
-package com.java;
-
-public class Strings_03 {
-    public static void main(String[] args) {
-        String message = "    Hello World" + "!!"   ;
-        System.out.println(message.trim());
-        System.out.println(message);
-    }
-}
