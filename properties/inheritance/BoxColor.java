343071240f0e491eeb040faf8c7924325c68a42c
VEDANT B <mevedantbhojne@gmail.com>
2025-02-12 03:06:29 +0530

    Delete properties directory

--- properties/inheritance/BoxColor.java
+++
@@ -1,6 +0,0 @@
-package com.java.properties.inheritance;
-
-public class BoxColor extends BoxWeight {
-    // Just make many classes and do it like previously we've done for BoxPrice
-    // That will be the Hierarchical Inheritance
-}
--- properties/polymorphism/Square.java
+++
@@ -1,7 +0,0 @@
-package com.java.properties.polymorphism;
-
-public class Square extends Shapes {
-    void area() {
-        System.out.println("Area is square of side");
-    }
-}
